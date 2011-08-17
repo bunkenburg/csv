0030826d94e15ac9e4bbbbde902977becf7ce5ab
Alexander Bunkenburg <alex@inspiracio.com>
2011-08-18 01:28:19 +0200

    add license

--- src.test/inspiracio/io/CSVReaderTest.java
+++ src.test/inspiracio/io/CSVReaderTest.java
@@ -1,3 +1,20 @@
+/*  Copyright 2011 Alexander Bunkenburg alex@inspiracio.com
+
+    This file is part of csv.
+
+    csv is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    csv is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with csv.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package inspiracio.io;
 
 import static org.junit.Assert.assertEquals;
@@ -9,7 +26,7 @@ import java.net.URL;
 
 import org.junit.Test;
 
-public class CSVReaderTest {
+public class CSVReaderTest{
 	
 	@Test public void trac_6030() throws IOException {
 		String filePath=getDirectoryPath(this.getClass()) + File.separator + "6030.csv";
